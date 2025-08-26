f8bd2f42c162491ad53a6a692623571a6c660436
Renren-lang <renielcornitez1@gmail.com>
2025-08-26 08:27:06 +0800

    index and create were changed

--- app/goals/create.jsx
+++ app/goals/create.jsx
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 18,
-    backgroundColor: '#21cc8d',
+    backgroundColor: '#6984ddff',
     color: 'white',
     borderRadius: 8,
   }
